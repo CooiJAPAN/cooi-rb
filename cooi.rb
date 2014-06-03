689f3fb17dca70708d060b9bd35e6ff06832ea46
MANJOME, Yoshikimi <manpoko@outlook.com>
2014-06-03 22:48:02 +0900

    First commit.

---
+++ cooi.rb
@@ -0,0 +1,45 @@
+#!/usr/bin/env ruby
+#
+#
+#
+class Hirase
+  @@letters ||= ['c', 'o', 'i', 's', '']
+  @@cooi_words ||= %w(Cooi Cisco Sicco Siso Oisii Isoiso)
+
+  class << self
+    def random_letter
+      @@letters.sample
+    end
+
+    def length
+      rand(3) + rand(3) + 1
+    end
+
+    def cooi?(its)
+      @@cooi_words.include? its
+    end
+
+    def shout(word)
+      puts "" 
+      puts "" 
+      puts "    　＿人人人人人人人人人＿ "
+      puts "    　＞　 #{word} JAPAN! 　＜"
+      puts "    　￣^Y^Y^Y^Y^Y^Y^Y^Y^Y￣ "
+    end
+    
+    def main
+      loop do
+        word = length.times.reduce('') { |w| w + random_letter }.capitalize!
+
+        printf "#{word} "
+        if cooi? word
+          shout word
+          break
+        end
+      end
+    end
+  end
+
+end
+
+Hirase.main
